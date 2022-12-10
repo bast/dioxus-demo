e85c49b926ee580abb05a94d9c1d76bb4ce53b7b
Radovan Bast <bast@users.noreply.github.com>
2022-12-10 22:02:54 +0100

    example code

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use dioxus::prelude::*;
+use gloo_timers::future::TimeoutFuture;
+
+fn main() {
+    dioxus::web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    let mut count = use_state(&cx, || 0);
+
+    use_future(&cx, (), move |_| {
+        let mut count = count.clone();
+        async move {
+            loop {
+                TimeoutFuture::new(3_000).await;
+                count += 1;
+            }
+        }
+    });
+
+    cx.render(rsx! {
+        h1 { "counter: {count}" }
+        button { class: "uk-button uk-button-primary",
+                 onclick: move |_| count += 1, "more" }
+        button { class: "uk-button uk-button-secondary",
+                 onclick: move |_| count -= 1, "less" }
+        button { class: "uk-button uk-button-danger",
+                 onclick: move |_| count.set(0), "reset" }
+    })
+}
