66a3f04c0635d9d5d7c2ed2e970dd8e2dfd39e1b
Radovan Bast <bast@users.noreply.github.com>
2022-12-15 06:30:41 +0100

    improve toggle example

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
 fn app(cx: Scope) -> Element {
     let mut count = use_state(&cx, || 0);
-    let mut toggle_is_active = use_state(&cx, || true);
+    let disabled = use_state(&cx, || false);
 
     use_future(&cx, (), move |_| {
         let mut count = count.clone();
@@ -21,14 +21,17 @@ fn app(cx: Scope) -> Element {
 
     cx.render(rsx! {
         h1 { "counter: {count}" }
-        p { "toggle_is_active: {toggle_is_active}" }
+        p { "disabled: {disabled}" }
         button { class: "uk-button uk-button-primary uk-margin-small-right",
-                 onclick: move |_| toggle_is_active.set(!toggle_is_active), "toggle" }
+                 onclick: move |_| disabled.set(!disabled), "toggle" }
         button { class: "uk-button uk-button-primary uk-margin-small-right",
                  onclick: move |_| count += 1, "more" }
         button { class: "uk-button uk-button-secondary uk-margin-small-right",
                  onclick: move |_| count -= 1, "less" }
-        button { class: "uk-button uk-button-danger uk-margin-small-right",
-                 onclick: move |_| count.set(0), "reset" }
+        button {
+            disabled: "{disabled}",
+            class: "uk-button uk-button-danger uk-margin-small-right",
+            onclick: move |_| count.set(0), "reset"
+        }
     })
 }
