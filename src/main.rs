c869ed0d4dc6842389813e65d9eedb369ad363c0
Radovan Bast <bast@users.noreply.github.com>
2022-12-15 05:45:18 +0100

    add toggle

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
 
 fn app(cx: Scope) -> Element {
     let mut count = use_state(&cx, || 0);
+    let mut toggle_is_active = use_state(&cx, || true);
 
     use_future(&cx, (), move |_| {
         let mut count = count.clone();
@@ -20,6 +21,9 @@ fn app(cx: Scope) -> Element {
 
     cx.render(rsx! {
         h1 { "counter: {count}" }
+        p { "toggle_is_active: {toggle_is_active}" }
+        button { class: "uk-button uk-button-primary uk-margin-small-right",
+                 onclick: move |_| toggle_is_active.set(!toggle_is_active), "toggle" }
         button { class: "uk-button uk-button-primary uk-margin-small-right",
                  onclick: move |_| count += 1, "more" }
         button { class: "uk-button uk-button-secondary uk-margin-small-right",
